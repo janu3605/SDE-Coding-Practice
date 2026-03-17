aad025cda574afbbf1ceec7ecef6e6246de9d755
Jaswanth <janu3605a@gmail.com>
2026-03-17 11:18:39 +0530

    code - 1

--- Greedy/TelecomCoverage.java
+++ Greedy/TelecomCoverage.java
@@ -3,15 +3,54 @@
      PROBLEM: Telecom Tower Coverage
      A telecom company wants to place towers to cover a set of cities along a straight line.
      Each tower has a fixed coverage radius. Determine the minimum number of towers needed to
-     cover all cities.
-    
+     cover all cities ,towers to sit directly on top of an existing house.
+
      INPUT:
      - cityPositions (array of integers, sorted or unsorted)
      - towerRadius (integer)
-    
+
+         n = 5, k = 2, houses = [1, 2, 5, 8, 10]
+
+
      OUTPUT:
      - Return an int representing the minimum number of towers required.
+         2
+
  */
+
+import java.util.*;
+
 public class TelecomCoverage {
-    // Your Java logic goes here
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] houses = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(houses);
+
+        int coverage = sc.nextInt();
+
+        TowerPlaced(houses, coverage);
+
+    }
+
+    public static void TowerPlaced(int[] houses, int coverage) {
+        int i = 0;
+        ArrayList<Integer> towerLoc = new ArrayList<>();
+
+        while (i < houses.length) {
+            int startHouse = houses[i];
+
+            while (i < houses.length && houses[i] <= startHouse + coverage) {
+                i++;
+            }
+            int towerloc = houses[i - 1];
+            towerLoc.add(towerloc);
+
+            while (i < houses.length && houses[i] <= towerloc + coverage) {
+                i++;
+            }
+        }
+        System.out.println(towerLoc);
+    }
 }
--- SlidingWindow/CheckoutBuffer.java
+++ SlidingWindow/CheckoutBuffer.java
@@ -1,22 +1,54 @@
+
 /**
  * MODEL: Sliding Window
  * PROBLEM: Automated Checkout Buffer
- * A major supermarket chain is upgrading its automated checkout systems. 
+ * A major supermarket chain is upgrading its automated checkout systems.
  * The conveyor belt brings items to the scanner one by one, represented by an array of their weights.
  * The new scanner has a temporary memory buffer that can only handle a maximum total weight, `W`, at any given time.
  * To keep the belt moving smoothly and avoid system crashes, the scanner must process a *contiguous* sequence of items.
  *
- * INPUT: 
+ * INPUT:
  * - weights (array of integers)
  * - maxWeight (integer)
  *
- * OUTPUT: 
+ * OUTPUT:
  * - Return an `int` representing the maximum number of consecutive items that can be scanned.
  *
  * EXAMPLE:
  * Input: weights = [3, 1, 2, 4, 2, 1, 1, 5], maxWeight = 8
  * Output: 4 (The sequence [4, 2, 1, 1] has a weight of 8 and length 4)
  */
+import java.util.*;
+
 public class CheckoutBuffer {
-    // Your Java logic goes here
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] weights = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int maxWt = sc.nextInt();
+
+        System.out.println(Maxitems(weights, maxWt));
+
+        sc.close();
+    }
+
+    public static ArrayList Maxitems(int[] weights, int maxWt) {
+        Arrays.sort(weights);
+
+        ArrayList<Integer> weight = new ArrayList<>();
+
+        int wt = 0;
+        for (int ele : weights) {
+            if (wt + ele > maxWt) {
+                break;
+            }
+            wt += ele;
+            weight.add(ele);
+        }
+        return weight;
+
+    }
 }
